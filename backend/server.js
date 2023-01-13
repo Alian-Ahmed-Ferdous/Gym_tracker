204e1f854093d6cf2f94b0c3a5a1ba0c74e77f35
alian-ahmed-ferdous <alianahmedferdous00@gmail.com>
2023-01-13 12:08:07 +0600

    add user model and controller

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const workoutsRoutes = require('./routes/workouts')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
@@ -10,23 +11,23 @@ const app = express()
 // middleware
 app.use(express.json())
 
-app.use((req ,res, next) => {
-    console.log(req.path, req.method)
-    next()
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
 
 // routes
-app.use('/api/workouts', workoutsRoutes)
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
-mongoose.connect(process.env.MONG_URL)
-    .then(() => {
-        // listen for requests
-        app.listen(process.env.PORT, () => {
-        console.log('listening to port ', process.env.PORT)
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
     })
-    })
-    .catch((error) => {d
-        console.log(error)
-    })
-
+  })
+  .catch((error) => {
+    console.log(error)
+  })
